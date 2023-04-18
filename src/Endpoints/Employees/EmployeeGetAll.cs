bd29a906ce9ba425e7159ac06f436336fbb57ca7
Andre Francez <andregfrancez@gmail.com>
2023-04-18 19:30:01 -0300

    Add Identity with Dapper

---
+++ src/Endpoints/Employees/EmployeeGetAll.cs
@@ -0,0 +1,19 @@
+﻿using WebApiUdemy.Infra.Data;
+
+namespace WebApiUdemy.Endpoints.Employees;
+
+public class EmployeeGetAll {
+    public static string Template => "/employees";
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+    public static Delegate Handle => Action;
+
+    public static IResult Action(int? page, int? rows, QueryAllUsersWithClaimName query) {
+        if (page == null)
+            page = 1;
+
+        if (rows == null || rows < 1 || rows > 10)
+            rows = 10;
+
+        return Results.Ok(query.Execute(page.Value, rows.Value));
+    }
+}
